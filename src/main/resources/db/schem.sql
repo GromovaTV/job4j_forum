04cb0a5659ffb680c24df096298ebccf6fb81f0d
Tatyana <quininthenorth@gmail.com>
2023-10-25 11:53:09 +0400

    Spring Liquibase

--- src/main/resources/db/schem.sql
+++ src/main/resources/db/schem.sql
@@ -4,10 +4,3 @@ name varchar(2000),
 description text,
 created timestamp without time zone not null default now()
 );
-
-create table IF NOT EXISTS users (
-id serial primary key,
-password varchar(100) NOT NULL,
-username varchar(50) NOT NULL unique,
-enabled boolean
-);
